ef6faea46f5655f9b94c122b09a72753c706798c
Alfonsodan <danalfons@yahoo.com>
2015-12-09 01:12:25 -0500

    added assets, scripts for movement and animation.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private Animator playerAnimator;
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+		playerAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveHorizontal = Input.GetAxisRaw("Horizontal");
+		moveVertical = Input.GetAxisRaw("Vertical");
+
+		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+	}
+
+	void FixedUpdate() {
+
+	}
+
+}

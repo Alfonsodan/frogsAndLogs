b491f528dbde4701c268654c26f1feb139de81d7
Alfonsodan <danalfons@yahoo.com>
2015-12-09 19:51:40 -0500

    added script for player control

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
@@ -6,22 +6,36 @@ public class PlayerMovement : MonoBehaviour {
 	private float moveHorizontal;
 	private float moveVertical;
 	private Vector3 movement;
+	private float turningSpeed = 20f;
+	private Rigidbody playerRigidbody;
 
 	// Use this for initialization
 	void Start () {
+
+		// Gather componants from the player game object
 		playerAnimator = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// gather input from the keyboard
 		moveHorizontal = Input.GetAxisRaw("Horizontal");
 		moveVertical = Input.GetAxisRaw("Vertical");
 
 		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 	}
 
-	void FixedUpdate() {
+	void FixedUpdate () {
 
+		// if the players movement vector doesnt = zero...
+		if (movement != Vector3.zero) {
+			// ... than play the jump animation
+			playerAnimator.SetFloat ("Speed", 3f);
+		} else {
+			// ... otherwise dont play the jump animation
+			playerAnimator.SetFloat ("Speed", 0f);
 	}
-
+	}
 }

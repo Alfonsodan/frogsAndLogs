5dcc45f8064f8f30251fa3399e611e8792edf629
Alfonsodan <danalfons@yahoo.com>
2015-12-12 00:54:30 -0500

    Added pickup objects scrips.

---
+++ Assets/Scripts/FlyMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform center;
+
+	private float flySpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		// the Random class is built in that has a method named range, range picks a value between 2 floats ...
+		flySpeed = Random.Range (300f, 700f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// makes the flys random ...
+		transform.RotateAround (center.position, Vector3.up, flySpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+
+		// if the collider "other" is tagged with "player"
+		if (other.CompareTag ("Player")) {
+
+			Destroy (gameObject);
+		}
+
+	}
+
+}
